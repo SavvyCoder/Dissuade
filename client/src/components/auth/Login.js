432cdb0b6397f8d5d4c03ce43fe6b77c6de2426a
Savinda Gunawardena <caladedge@gmail.com>
2018-09-04 10:09:42 +0000

    Before Redux implementation

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+import classnames from "classnames";
+
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      email: "example@email.com",
+      password: "",
+      errors: {}
+    };
+  }
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    const user = {
+      email: this.state.email,
+      password: this.state.password
+    };
+
+    console.log(user);
+  };
+
+  render() {
+    return (
+      <div className="login">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Log In</h1>
+              <p className="lead text-center">
+                Sign in to your Dissuade account
+              </p>
+              <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <input
+                    type="email"
+                    className="form-control form-control-lg"
+                    placeholder="Email Address"
+                    value={this.state.email}
+                    name="email"
+                    onChange={this.onChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <input
+                    type="password"
+                    className="form-control form-control-lg"
+                    placeholder="Password"
+                    value={this.state.value}
+                    name="password"
+                    onChange={this.onChange}
+                  />
+                </div>
+                <input type="submit" className="btn btn-info btn-block mt-4" />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Login;
