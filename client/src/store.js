378e724bff3549759d7301128a266d33c44e753d
Savinda Gunawardena <caladedge@gmail.com>
2018-09-21 21:06:54 +0000

    removed redux_devtools from store.js

--- client/src/store.js
+++ client/src/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
-import { composeWithDevTools } from "redux-devtools-extension";
+// import { composeWithDevTools } from "redux-devtools-extension";
 
 const initialState = {};
 
@@ -12,7 +12,7 @@ const middleware = [thunk];
 const store = createStore(
   rootReducer,
   initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
+  applyMiddleware(...middleware)
 );
 
 export default store;
