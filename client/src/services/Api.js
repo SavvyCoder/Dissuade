7c62ab80301f7e6e76f6febada164bc771812db6
Savinda Gunawardena <caladedge@gmail.com>
2018-09-21 21:11:50 +0000

    initial APi config change

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default () => {
   return axios.create({
-    baseURL: `https://mern-dissuadev2-caladedge.c9users.io:8081`
+    baseURL: `https://dissuade.herokuapp.com`
   });
 };
